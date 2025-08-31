8226b2ed8b9e2988e6c075598cf89db737dce894
sarrthak008 <sarthaknavale08@gmail.com>
2025-08-31 18:58:42 +0300

    add a basci folder and card and some animation

---
+++ demo1/src/AnimateCard.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from 'react'
+import gsap from 'gsap'
+
+
+const AnimateCard = () => {
+
+    const [step,setStep] = useState(0)
+
+    const handelClick =()=>{
+
+         if(step == 0){
+            gsap.to('#silde-circle',{
+                translateX:"-0%",
+                left:"0%"
+            })
+            setStep(1)
+            return 
+         }
+
+        
+          if(step == 1){
+            gsap.to('#silde-circle',{
+                translateX:"-50%",
+                left:"50%"
+            })
+            setStep(2)
+            return
+          }
+
+          if(step == 2){
+              gsap.to('#silde-circle',{
+                translateX:"-100%",
+                left:"100%"
+            })
+            setStep(0)
+            return
+          }
+
+    }
+
+
+  return (
+    <div className='h-[450px] w-[350px] rounded-2xl overflow-hidden'>
+         <div className='h-[60%] w-full bg-orange-400'></div>
+         <div className='h-[40%] w-full bg-white'>
+             <div className='h-[40%] flex items-center w-[96%] mx-auto' onClick={()=>handelClick()}>
+                 <div className='h-[80px] w-[80px] bg-white rounded-full relative top-[-40%] flex items-center justify-center ' id='silde-circle' ></div>
+             </div>
+             <div className='h-[30%]  w-full mb-20 py-[4px] flex items-center justify-center gap-[25px]'>
+                 <div className='h-full w-[8px] bg-black rounded-2xl'></div>
+                     <div className='h-[40%] w-[7px] bg-gray-500 rounded-2xl'></div>
+                     <div className='h-[40%] w-[7px] bg-gray-500 rounded-2xl'></div>
+                     <div className='h-[40%] w-[7px] bg-gray-500 rounded-2xl'></div>
+                 <div className='h-full w-[8px] bg-black rounded-2xl'></div>
+                      <div className='h-[40%] w-[7px] bg-gray-500 rounded-2xl'></div>
+                      <div className='h-[40%] w-[7px] bg-gray-500 rounded-2xl'></div>
+                      <div className='h-[40%] w-[7px] bg-gray-500 rounded-2xl'></div>
+                 <div className='h-full w-[8px] bg-black rounded-2xl'></div>
+
+             </div>
+         </div>
+    </div>
+  )
+}
+
+export default AnimateCard
---
+++ demo1/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AnimateCard from './AnimateCard'
+
+const App = () => {
+  return (
+    <div className='h-screen w-screen overflow-hidden bg-gray-700  flex items-center justify-center'>
+        <AnimateCard/>
+    </div>
+  )
+}
+
+export default App
